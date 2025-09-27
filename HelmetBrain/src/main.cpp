87c809cac9612e129ab7f1af198f0eb69c0fa502
sreekara <sreedansd@gmail.com>
2025-09-26 22:59:49 -0400

    fixed platformio init for our specific board

--- HelmetBrain/src/main.cpp
+++ HelmetBrain/src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "wap";
+const char *password = "password";
+
+WiFiServer server(80);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println();
+  Serial.println("Configuring platforimo Access Point...");
+
+  // Explicit channel 1 to improve visibility
+  if (WiFi.softAP(ssid, password, 1)) {
+    Serial.println("AP Created Successfully!");
+    Serial.print("AP IP address: ");
+    Serial.println(WiFi.softAPIP());
+  } else {
+    Serial.println("AP Creation Failed!");
+    while (true) { delay(1000); } // halt if AP fails
+  }
+
+  server.begin();
+  Serial.println("HTTP Server started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
