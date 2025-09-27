506f0c38ce7700ac8ba44e4a461515b790a05258
Theo <theomhalpern@gmail.com>
2025-09-27 00:44:38 -0400

    merged

--- HelmetBrain/src/main.cpp
+++ HelmetBrain/src/main.cpp
@@ -15,9 +15,30 @@ const char *password = "helmet123";
 void startCameraServer();
 void setupLedFlash();
 
+/*
+  Ellipse drawing example
+
+  This sketch does not use any fonts.
+*/
+
+#include <SPI.h>
+
+#include <TFT_eSPI.h> // Hardware-specific library
+
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+
 void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
+  delay(1000);
+
+  tft.init();
+
+  tft.setRotation(1);
+
+
   Serial.println();
 
   camera_config_t config;
@@ -116,5 +137,30 @@ void setup() {
 void loop() {
   // Do nothing. Everything is done in another task by the web server
   delay(10000);
-}
 
+    tft.fillScreen(TFT_BLACK);
+
+  // Draw some random circles
+  for (int i = 0; i < 40; i++)
+  {
+    int rx = random(60);
+    int ry = random(60);
+    int x = rx + random(480 - rx - rx);
+    int y = ry + random(320 - ry - ry);
+    tft.fillEllipse(x, y, rx, ry, random(0xFFFF));
+  }
+
+  delay(2000);
+  tft.fillScreen(TFT_BLACK);
+
+  for (int i = 0; i < 40; i++)
+  {
+    int rx = random(60);
+    int ry = random(60);
+    int x = rx + random(480 - rx - rx);
+    int y = ry + random(320 - ry - ry);
+    tft.drawEllipse(x, y, rx, ry, random(0xFFFF));
+  }
+
+  delay(2000);
+}
